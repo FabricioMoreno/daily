4af8a6ee491c0362bb50d11ce193738c0c9ebc16
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-01-06 07:46:03 -0500

    day0

---
+++ day0.js
@@ -0,0 +1,21 @@
+//Fizz Buzz
+
+const FizzBuzz = (n) => {
+  const result = [];
+  if (n <= 1) {
+    return "N is invalid";
+  }
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      result.push("Fizz");
+    } else if (i % 5 == 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};
+console.log(FizzBuzz(16));
